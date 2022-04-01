96b362c673481298e0ccee9c45ae3fca48f8bd68
kharriiTD <carina.td@hotmail.com>
2022-04-01 19:46:18 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to my quiz game! ")
+
+player = input(" Do you want to play? ")
+if player != "yes" :
+   quit()
+   
