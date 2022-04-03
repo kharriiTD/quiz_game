d6dbbeccf2c72b74303e0af2def92f626ec27250
kharriiTD <carina.td@hotmail.com>
2022-04-03 08:23:19 +0000

    added questions and options to the game

--- run.py
+++ run.py
@@ -10,3 +10,28 @@ def display_score():
 def play_agian():
    pass
 
+#questions
+
+question = {
+   "How long is the gestation period of an African elephant?:": "A",
+   "What’s the scientific name of a wolf?:": "C",
+   "What is a female donkey called?:": "B",
+   "Which mammal has no vocal cords?:": "B",
+   "What’s the fastest land animal in the world?:": "A",
+   "How many eyes does a bee have?:": "C",
+   "Which animal symbolizes good luck in Europe?:": "B",
+   "What name is used to refer to a group of frogs?:": "C",
+   "How many hearts does an octopus have?:": "A",
+   "How many compartments are in a cow's stomach:": "B",
+}
+
+options = [["A. 22 month", "B. 24 month", "C. 18 month"],
+           ["A. Candiz Lupra", "B. Cardo Lapo", "C. Canis Lupus"],
+           ["A. A Sendra", "B. A Jenny", "C. A Sara"],
+           ["A. Tiger", "B. Giraffe", "C. Cat"],
+           ["A. The cheetah ", "The lion", "C. The horse"],
+           ["A. Four", "B. Six", "C. Five"],
+           ["A. Bee", "B. Ladybug", "C. Butterfly"],
+           ["A. A family", "B. A stuff", "C. An army"],
+           ["A. Three", "B. Five", "C. 18 One"],
+           ["A. Two", "B. Four", "C. Six"]]
